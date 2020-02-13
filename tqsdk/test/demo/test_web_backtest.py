ec4e4c6549f068d3b813453ce6bbf277218896f9
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-02-13 20:32:01 +0800

    fix: 添加 web_gui 测试用例

---
+++ tqsdk/test/demo/test_web_backtest.py
@@ -0,0 +1,109 @@
+#!usr/bin/env python3
+#-*- coding:utf-8 -*-
+"""
+@author: yanqiong
+@file: test_web.py
+@create_on: 2020/2/12
+@description: "Users/yanqiong/Documents/geckodriver-v0.26.0-macos.tar.gz"
+"""
+import os
+import sys
+import time
+import unittest
+from multiprocessing import Process
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import date
+from tqsdk import TqApi, TqBacktest, TargetPosTask
+
+# 子进程要执行的代码
+def run_tianqin_code(port):
+    try:
+        api = TqApi(backtest=TqBacktest(start_dt=date(2018, 5, 5), end_dt=date(2018, 5, 10)), web_gui=":" + port)
+        klines = api.get_kline_serial("DCE.m1901", 5 * 60, data_length=15)
+        target_pos = TargetPosTask(api, "DCE.m1901")
+        while True:
+            api.wait_update()
+            if api.is_changing(klines):
+                ma = sum(klines.close.iloc[-15:]) / 15
+                if klines.close.iloc[-1] > ma:
+                    target_pos.set_target_volume(5)
+                elif klines.close.iloc[-1] < ma:
+                    target_pos.set_target_volume(0)
+
+    except Exception as e:
+        api.close()
+
+class WebBacktestFirefoxTest(unittest.TestCase):
+
+    def setUp(self):
+        self.port = "9877"
+        self.tq_process = Process(target=run_tianqin_code, args=(self.port, ))
+        self.tq_process.start()
+
+    def tearDown(self):
+        self.tq_process.terminate()
+
+    def test_web_firefox(self):
+        if sys.platform.startswith("win"):
+            exe_path = os.path.join(os.getenv("GeckoWebDriver"), "geckodriver.exe")
+        else:
+            return
+        driver = webdriver.Firefox(executable_path=exe_path)
+        run_for_driver(driver, self)
+
+class WebBacktestChromeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.port = "9878"
+        self.tq_process = Process(target=run_tianqin_code, args=(self.port, ))
+        self.tq_process.start()
+
+    def tearDown(self):
+        self.tq_process.terminate()
+
+    def test_web_chrome(self):
+        if sys.platform.startswith("win"):
+            exe_path = os.path.join(os.getenv("ChromeWebDriver"), "chromedriver.exe")
+        elif sys.platform.startswith("darwin"):
+            exe_path = os.path.join(os.getcwd(), "chromedriver")
+        else:
+            return
+        driver = webdriver.Chrome(executable_path=exe_path)
+        run_for_driver(driver, self)
+
+def run_for_driver(driver, test):
+    time.sleep(10)
+    driver.implicitly_wait(30)
+    driver.get("http://127.0.0.1:" + test.port)
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.title_is("tqsdk-python-web"))  # k线图显示
+    logo = driver.find_element_by_tag_name("img")
+    test.assertEqual("Tianqin", logo.get_attribute("alt"))
+    # K线是否有成交箭头
+    chart_main_marks = driver.find_element_by_css_selector("svg.tqchart>g.root g.main.marks")
+    trade_arrow_paths = chart_main_marks.find_element_by_css_selector("g.tradearrow")
+    wait = WebDriverWait(driver, 10)
+    wait.until(element_has_child(trade_arrow_paths, "path"))
+    # 成交列表是否显示
+    trades_table = driver.find_element_by_css_selector("div.reports.trades-table>table")
+    wait = WebDriverWait(driver, 30)
+    wait.until(element_has_child(trades_table, "tbody>tr"))
+    driver.close()
+
+class element_has_child(object):
+    def __init__(self, element, css_selector):
+        self.element = element
+        self.css_selector = css_selector
+
+    def __call__(self, driver):
+        children = self.element.find_element_by_css_selector(self.css_selector)
+        print("children", children)
+        if not children:
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    unittest.main()
